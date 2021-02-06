df6b3fc17ab1f886abf5ad56dd95be0d3e2f9508
mac <2579300440@qq.com>
2021-02-06 17:52:39 +0800

    ---

--- src/gtiDemo/Jd.java
+++ src/gtiDemo/Jd.java
@@ -6,12 +6,14 @@ public class Jd {
 
 	public void name() {
 		/* 建立一个Collection */
-		 String[] strings = {"A", "B", "C", "D","E"};
+		 String[] strings = {"A", "B", "C","234234", "D","E"};
+		 System.out.println(strings+"233453432423"); /* 依次输出“A”、“B”、“C”、“D” */
 		 Collection listS = java.util.Arrays.asList(strings);
      
 		 
 		 /* 开始遍历 12312s */
 		 for (Object str : listS) {
+			 System.out.println(str+"Z张"); /* 依次输出“A”、“B”、“C”、“D” */
 		     System.out.println(str+"233453432423"); /* 依次输出“A”、“B”、“C”、“D” */
 		 }
 	}
