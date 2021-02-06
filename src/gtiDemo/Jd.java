15957f9bedad4eb173eb6eeedb68760e05f86054
mac <2579300440@qq.com>
2021-02-06 17:14:06 +0800

    ---

--- src/gtiDemo/Jd.java
+++ src/gtiDemo/Jd.java
@@ -8,8 +8,9 @@ public class Jd {
 		/* 建立一个Collection */
 		 String[] strings = {"A", "B", "C", "D"};
 		 Collection list = java.util.Arrays.asList(strings);
-
-		 /* 开始遍历 */
+     
+		 
+		 /* 开始遍历 12312s */
 		 for (Object str : list) {
 		     System.out.println(str); /* 依次输出“A”、“B”、“C”、“D” */
 		 }
