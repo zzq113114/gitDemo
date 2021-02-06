397fd840d9eec808beed26816bcf48715e216109
mac <2579300440@qq.com>
2021-02-06 17:16:34 +0800

    ---

--- src/gtiDemo/Jd.java
+++ src/gtiDemo/Jd.java
@@ -15,4 +15,15 @@ public class Jd {
 		     System.out.println(str); /* 依次输出“A”、“B”、“C”、“D” */
 		 }
 	}
+	public void name2() {
+		/* 建立一个Collection */
+		 String[] strings = {"A", "B", "C", "D"};
+		 Collection list = java.util.Arrays.asList(strings);
+     
+		 
+		 /* 开始遍历 12312s */
+		 for (Object str : list) {
+		     System.out.println(str); /* 依次输出“A”、“B”、“C”、“D” */
+		 }
+	}
 }
