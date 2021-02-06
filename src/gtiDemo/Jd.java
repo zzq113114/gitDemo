abf50a1abed096b813d1a9bfa65900416f928b09
mac <2579300440@qq.com>
2021-02-06 17:07:46 +0800

    --

---
+++ src/gtiDemo/Jd.java
@@ -0,0 +1,17 @@
+package gtiDemo;
+
+import java.util.Collection;
+
+public class Jd {
+
+	public void name() {
+		/* 建立一个Collection */
+		 String[] strings = {"A", "B", "C", "D"};
+		 Collection list = java.util.Arrays.asList(strings);
+
+		 /* 开始遍历 */
+		 for (Object str : list) {
+		     System.out.println(str); /* 依次输出“A”、“B”、“C”、“D” */
+		 }
+	}
+}
