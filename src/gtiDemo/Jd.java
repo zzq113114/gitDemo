b3c15d63a21dcbe9672b4065360853c061d042dd
mac <2579300440@qq.com>
2021-02-07 22:23:04 +0800

    NBS -23239 自主出单优化

--- src/gtiDemo/Jd.java
+++ src/gtiDemo/Jd.java
@@ -4,9 +4,21 @@ import java.util.Collection;
 
 public class Jd {
 
-	public void name() {
+	public void age() {
 		/* 建立一个Collection */
 		 String[] strings = {"A", "B", "C", "D"};
+		 Collection list = java.util.Arrays.asList(strings);
+     
+		 
+		 /* 开始遍历 12312s */
+		 for (Object str : list) {
+		     System.out.println(str); /* 依次输出“A”、“B”、“C”、“D” */
+		 }
+	}
+	
+	public void higeht() {
+		/* 建立一个Collection */
+		 String[] strings = {"A", "E", "C", "D"};
 		 Collection list = java.util.Arrays.asList(strings);
      
 		 
